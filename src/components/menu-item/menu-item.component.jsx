581b80b948cccadf22ec56ec40414cb44b6e3a0d
Niall Whelan <niallwhelan2010@hotmail.com>
2021-12-28 15:02:47 +0000

    Update our menu-items to navigate with react-router

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+    let navigate = useNavigate();
     return (
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} onClick={() => navigate(linkUrl)}>
             <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
             <div className="content">
                 <h1 className="title">{title.toUpperCase()}</h1>
